1a67e9a0fa3ea0173dee834d1aa4fd616716c16c
Juwan Lee <ljw05w5@gmail.com>
2023-06-12 12:17:55 +0900

    MDI 인터페이스 알아보기

--- ModalModelessPrac/Form1.cs
+++ ModalModelessPrac/Form1.cs
@@ -24,6 +24,7 @@ namespace ModalModelessPrac
         public Form1()
         {
             InitializeComponent();
+            IsMdiContainer = true;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -47,6 +48,7 @@ namespace ModalModelessPrac
         private void button2_Click(object sender, EventArgs e)
         {
             CustomForm form = new CustomForm();
+            form.MdiParent = this;
             form.Show();
         }
     }
